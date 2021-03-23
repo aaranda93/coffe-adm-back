3369ca5c28d6da6f66eeb9c211099c0452fe3f68
Alexis Aranda <aaranda@jumpitt.com>
2021-03-22 22:20:08 -0300

    products controller creation

---
+++ app/Http/Controllers/V1/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Modelsl\Product;
+
+class ProductController extends Controller
+{
+    /**
+     * Retrieve the user for the given ID.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+
+}
