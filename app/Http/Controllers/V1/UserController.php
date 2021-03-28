c042d6d8da56de329266838bf6639d4623d86e41
Alexis Aranda <aaranda@jumpitt.com>
2021-03-27 21:06:08 -0300

    feat: create user controller

---
+++ app/Http/Controllers/V1/UserController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Modelsl\User;
+
+class UserController extends Controller
+{
+    /**
+     * Retrieve the user for the given ID.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+       
+
+    }
+    public function show($user_id)
+    {
+       
+        
+    }
+    public function store(Request $request, $user_id)
+    {
+       
+        
+    }
+    public function update(Request $request, $user_id)
+    {
+       
+        
+    }
+    public function destroy($user_id)
+    {
+       
+        
+    }
+}
