00ed643724c5621a0a098b59f20d2500c7082ae2
Alexis Aranda <aaranda@jumpitt.com>
2021-03-30 19:11:08 -0300

    feat: products controller

--- app/Http/Controllers/V1/ProductController.php
+++ app/Http/Controllers/V1/ProductController.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Http\Controllers\V1;
-
 use Illuminate\Http\Request;
+use App\Http\Requests\Products\Store as StoreProduct;
+use App\Http\Requests\Products\Update as UpdateProduct;
+use App\Http\Requests\Products\Index as IndexProduct;
+use App\Http\Requests\Products\Show as ShowProduct;
+use App\Http\Constants\ApiResponse as Api;
 use App\Http\Controllers\Controller;
-use App\Modelsl\Product;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
@@ -14,27 +18,36 @@ class ProductController extends Controller
      * @param  int  $id
      * @return Response
      */
-    public function index(Request $request)
+    public function index(IndexProduct $request)
     {
-       
+        $branches = Product::get();
 
+        return $this->response(Api::OK, $branches);
     }
-    public function show($product_id)
+
+    public function show(ShowProduct $reques, $branch_id)
     {
-       
-        
+        $branch = Product::find($branch_id);
+
+        return $this->response(Api::OK, $branch);
     }
-    public function store(Request $request, $product_id)
+
+    public function store(StoreProduct $request)
     {
-       
-        
+        $newbranch = Product::create($request->all());
+
+        return $this->response(Api::CREATED, $newbranch);
     }
-    public function update(Request $request, $product_id)
+
+    public function update(UpdateProduct $request, $branch_id)
     {
-       
-        
+        $branch = Product::find($branch_id);
+        $branch->update($request->all());
+
+        return $this->response(Api::OK, $branch);
     }
-    public function destroy($product_id)
+
+    public function destroy($branch_id)
     {
        
         
